1e967f45b6c09b297ebb900c40aa0e9339e76ab0
smrev <sararevord@unomaha.edu>
2022-05-12 18:15:53 -0500

    Changed the game to allow for only one guess, and display the correct answer after each guess

--- mainJumble.py
+++ mainJumble.py
@@ -1,20 +1,23 @@
 import tkinter
-from tkinter import*
+from tkinter import *
 import random
 from tkinter import messagebox
 from random import shuffle
 import csv
 
+# open and read file that contains US President names
 presidents = open('us_presidents.csv', 'r')
-
 file = csv.DictReader(presidents)
 
+# variables to hold answers, jumbled answers,
 answers = []
+words = []
 
+
+# add names to the list containing game answers
 for column in file:
     answers.append(column['president'])
 
-words = []
 
 for i in answers:
     i = i.strip().lower().replace(' ', '')
@@ -30,6 +33,9 @@ def reset():
     num = random.randint(0, len(words)-1)
     lbl1.configure(text=words[num])
     e1.delete(0, END)
+    button2.forget()
+    button1.pack(pady=40)
+    button2.pack()
 
 
 def ans_check():
@@ -37,10 +43,11 @@ def ans_check():
     user_input = e1.get()
     if user_input == answers[num]:
         messagebox.showinfo('Success', 'Correct')
-        reset()
+        lbl1.configure(text=answers[num])
     else:
         messagebox.showinfo('Error', 'Try Again')
-        e1.delete(0, END)
+        lbl1.configure(text=answers[num])
+    button1.pack_forget()
 
 
 def initial():
