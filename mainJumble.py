37ac68ec50b3e23aceea59f5fc78214069e427db
smrev <sararevord@unomaha.edu>
2022-05-13 17:37:44 -0500

    Changed the game to allow for only one guess, and display the correct answer after each guess

--- mainJumble.py
+++ mainJumble.py
@@ -10,15 +10,17 @@ presidents = open('us_presidents.csv', 'r')
 file = csv.DictReader(presidents)
 
 # variables to hold answers, jumbled answers,
+# and keep track of the score
 answers = []
 words = []
-
+correct = 0
+total = 0
 
 # add names to the list containing game answers
 for column in file:
     answers.append(column['president'])
 
-
+#Jumble the game answers and place in separate list
 for i in answers:
     i = i.strip().lower().replace(' ', '')
     word = list(i)
@@ -27,49 +29,116 @@ for i in answers:
 
 num = random.randint(0, len(words)-1)
 
-
-def reset():
-    global words, num, answers
-    num = random.randint(0, len(words)-1)
-    lbl1.configure(text=words[num])
-    e1.delete(0, END)
-    button2.forget()
-    button1.pack(pady=40)
-    button2.pack()
+# function to control the next button
+def next():
+    global words, answers, num, total, correct
+    buttonNext.config(text = 'Next')
+    # enable the restart button in case players want to start over
+    buttonRestart.config(state='normal')
+    # disable the next button so players can't skip over a turn
+    buttonNext.config(state='disabled')
+    # update screen to reflect the score
+    labelNumCorrect.configure(text=correct)
+    labelNumTotal.configure(text=total)
+    # clear the entry field
+    userInput.delete(0, END)
+    # check to see if player has reached ten rounds
+    if total >= 10:
+        # at 10 rounds turn off check button
+        buttonCheck.config(state = 'disabled')
+        # Change label to final score
+        labelCorrect.configure(text='Final Correct')
+        # Ending message based on final score
+        if correct == 10:
+            labelTitle.configure(text='You are a Presidential Expert!')
+        elif 7 <= correct < 10:
+            labelTitle.configure(text='You know the Presidents Well!')
+        elif 4 <= correct < 7:
+            labelTitle.configure(text='At least you knew a few!')
+        else:
+            labelTitle.configure(text='Seems like you had some trouble!')
+        #reset scores
+        total = 0
+        correct = 0
+    # if under ten rounds, continue the game
+    else:
+        num = random.randint(0, len(words)-1)
+        labelTitle.configure(text=words[num])
+        buttonCheck.config(state='normal')
 
 
 def ans_check():
-    global words, num, answers
-    user_input = e1.get()
+    global words, num, answers, correct, total
+    buttonNext.config(state='normal')
+    if total == 0:
+        labelCorrect.configure(text='Total Correct')
+    total += 1
+    # check to see if answer is correct
+    user_input = userInput.get()
     if user_input == answers[num]:
+        correct += 1
         messagebox.showinfo('Success', 'Correct')
-        lbl1.configure(text=answers[num])
+        # update the message to show correct answer
+        labelTitle.configure(text=answers[num])
     else:
         messagebox.showinfo('Error', 'Try Again')
-        lbl1.configure(text=answers[num])
-    button1.pack_forget()
+        labelTitle.configure(text=answers[num])
+    # disable buttons so only button is next to start next round
+    buttonCheck.config(state = 'disabled')
+    buttonRestart.config(state = 'disabled')
 
+# function to restart the game
+def restart():
+    global correct, total
+    labelTitle.configure(text='Good Luck')
+    labelCorrect.configure(text='Total Correct')
+    buttonNext.config(text='Get President')
+    buttonNext.config(state = 'normal')
+    buttonCheck.config(state = 'disabled')
+    correct = 0
+    total = 0
+    labelNumCorrect.configure(text=correct)
+    labelNumTotal.configure(text=total)
 
+# function to initial screen
 def initial():
-    global words, num
-    lbl1.configure(text=words[num])
+    buttonCheck.config(state = 'disabled')
+    buttonRestart.config(state = 'disabled')
+    buttonNext.config(text = 'Start')
+    labelTitle.configure(text='Unscramble the US President\'s name!')
+    labelCorrect.configure(text='Can you get')
+    labelNumCorrect.configure(text='10')
+    labelTotal.configure(text='Out of')
+    labelNumTotal.configure(text='10')
 
-
-root = tkinter.Tk()
-root.geometry('500x300')
-lbl1 = Label(root, font='times 20')
-lbl1.pack(pady=30, ipady=10, ipadx=10)
+window = tkinter.Tk()
+window.geometry('500x500')
+window.resizable(False, False)
+labelTitle = Label(window, font='times 20')
+labelTitle.pack(pady=30, ipady=10, ipadx=10)
+labelCorrect = Label(window, font='times 14')
+labelNumCorrect = Label(window, font='times 14')
+labelTotal = Label(window, font='times 14')
+labelNumTotal = Label(window, font='times 14')
 
 answer12 = StringVar()
-e1 = Entry(root, textvariable=answer12)
-e1.pack(ipady=5, ipadx=5)
+userInput = Entry(window, textvariable=answer12)
+userInput.pack(ipady=5, ipadx=5)
 
-button1 = Button(root, text="Check", width=20, command=ans_check)
-button1.pack(pady=40)
+buttonCheck = Button(window, text="Check", width=20, command=ans_check)
+buttonCheck.pack(pady=40)
 
-button2 = Button(root, text='Reset', width=20, command=reset)
-button2.pack()
+buttonNext = Button(window, text='Next', width=20, command=next)
+buttonNext.pack()
+
+buttonRestart = Button(window, text='Restart', width=20, command=restart)
+
+labelCorrect.pack(pady=5)
+labelNumCorrect.pack(pady=5)
+labelTotal.pack(pady=5)
+labelNumTotal.pack(pady=5)
+buttonRestart.pack()
 
 initial()
 
-root.mainloop()
+window.mainloop()
